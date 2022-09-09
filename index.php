a8d763cca1aea336f731a1eaa54ab0d2090d295f
Radja Aulia Al Ramdani <radjaauliaalramdani@gmail.com>
2022-09-09 07:23:31 +0700

    Materi Web 8/9/22

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>XIRPL2</title>
+
+    <!-- Mengaitkan File CSS | href="namafile.css" sesuaikan dengan nama file css -->
+    <link rel="stylesheet" href="templates.css">
+</head>
+<body>
+    <!-- Struktur Web -->
+    <table width="600px" align="center">
+        <tr height="50px">
+            <td>
+                <a href="#">menu1</a>
+                <a href="#">menu2</a>
+                <a href="#">menu3</a>
+            </td>   
+        </tr>
+        <tr  height="250px">
+            <td>
+                <img src="istrigw.jpg" alt="istri" width="100%">
+            </td>
+        </tr>
+        <tr  height="550px">
+            <td></td>
+        </tr>
+    </table>
+</body>
+</html>
